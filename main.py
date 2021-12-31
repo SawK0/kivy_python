720a6d0d5f59917fc0b4f949d1e055cdaebbaa69
SawK0 <sawzawmyohtetko@gmail.com>
2021-12-31 16:22:39 +0700

    anchor layout

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 
 class BoxLayoutExample(BoxLayout):
